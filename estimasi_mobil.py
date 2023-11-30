143523ba563546e2c8bb4d9f9a4b9581a0c32cc4
kaz-y30 <152515362+kaz-y30@users.noreply.github.com>
2023-12-01 05:18:58 +0700

    Add files via upload

---
+++ estimasi_mobil.py
@@ -0,0 +1,21 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_mobil.sav', 'rb'))
+
+st.title('Estimasi Harga Mobil Bekas')
+
+year = st.number_input('Input Tahun Mobil')
+mileage = st.number_input('Input KM Mobil')
+tax = st.number_input('Input Pajak Mobil')
+mpg = st.number_input('Input Konsumsi BBm Mobil')
+engineSize = st.number_input('Input Engine Size')
+
+predict = ''
+
+if st.button('Estimasi Harga'):
+    predict = model.predict(
+        [[year, mileage, tax, mpg, engineSize]]
+    )
+    st.write ('Estimasi Harga Mobil Bekas Dalam Pounds adalah', predict)
+    st.write ('Estimasi Harga Mobil Bekas Dalam IDR (Juta) adalah', predict*19000)
